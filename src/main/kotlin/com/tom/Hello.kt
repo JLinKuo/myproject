7baf514492e14fbd96187fb41292a129ba688a85
yaerse <yaerse@yahoo.com.tw>
2018-12-19 18:33:46 +0800

    Java and Kotlin variable, data typ

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -6,6 +6,13 @@ fun main(args: Array<String>) {
 //    Human().hello()
     val h = Human()
     h.hello()
+
+    var age : Int = 19
+    age = 20
+
+    var weight = 66.5f
+
+    val name: String
 }
 
 class Human{
