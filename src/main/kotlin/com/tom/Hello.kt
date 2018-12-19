442b1e1a0e414fcf719061f201f5b338bf19a936
yaerse <yaerse@yahoo.com.tw>
2018-12-19 15:25:09 +0800

    Initial commit

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,15 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("Hello Kotlin")
+
+//    Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        print("hello kotlin")
+    }
+}
